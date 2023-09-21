67de0eb0292fb78f74fae108dc8843cec253c581
João Vitor <joaovitorso071@gmail.com>
2023-09-21 16:37:40 -0300

    modified:   .vscode/c_cpp_properties.json 	new file:   Aula02.cpp 	new file:   output/Aula02.exe

---
+++ Aula02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+//Posso acessar a bibliotea std, sem necessidade de prefixala.
+
+int main () {
+
+    cout << "Será que isso ainda funciona?";
+    return 0;
+    /*
+    Comentario também funciona dessa forma
+    
+    */
+
+    // Comentario é da mesma forma que C
+
+    /**
+     * 
+     * hmmmmmmmmmmmmmm, assim também funciona
+    */
+}
