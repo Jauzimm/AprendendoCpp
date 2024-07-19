13222773ee33948bcb7610eb022caa3cc3dbe3b8
Jauzimm <joaovitorso071@gmail.com>
2024-07-19 13:15:14 -0300

    testando

--- Aula15.cpp
+++ Aula15.cpp
@@ -9,12 +9,10 @@ int main () {
     // && two conditions is true
     // || At least one condition is true
     // ! Invert the logical
-
     if ( temp > 0 && temp < 30) {
         std::cout << "The temperature is good!";
     } else {
         std::cout << "The temperature is bad!";
     }
-
     return 0;
 }
